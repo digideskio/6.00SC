bb05b17a9c53d841bd380669430fe188d6d3cab7
Shouvik Roy <shouvik@techie.com>
2015-12-09 16:21:08 +0000

    Initial commit

---
+++ Unit 1/ps1a.py
@@ -0,0 +1,3 @@
+balance = float(raw_input("Enter the outstanding balance on your credit card:"))
+annual_interest_rate = float(raw_input("Enter the annual credit card interest rate as a decimal:"))
+minimum_monthly_pmt = float(raw_input("Enter the minimum monthly payment as a decimal"))
