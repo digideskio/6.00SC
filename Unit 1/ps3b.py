3d1cce0b513bb1bb966df313df9aaa5df8c34ee3
Shouvik Roy <shouvik@techie.com>
2015-12-11 12:02:32 +0530

    finised ps3b

--- Unit 1/ps3b.py
+++ Unit 1/ps3b.py
@@ -16,8 +16,16 @@ def comp_choose_word(hand, word_list):
     hand: dictionary (string -> int)
     word_list: list (string)
     """
-    # TO DO...
+    perm_list = []
+    for n in range(1, HAND_SIZE+1):
+        temp_list = get_perms(hand, n)
+        perm_list.extend(temp_list)
 
+    perm_list.sort(key=len, reverse = True)
+    for word in perm_list:
+        if word in word_list:
+            return word
+    return "."
 #
 # Problem #6B: Computer plays a hand
 #
@@ -40,7 +48,25 @@ def comp_play_hand(hand, word_list):
      hand: dictionary (string -> int)
      word_list: list (string)
     """
-    # TO DO ...    
+    total_score = 0
+    
+    while(calculate_handlen(hand) > 0): 
+        # Display hand
+        print "Current Hand:", 
+        display_hand(hand)
+        word = comp_choose_word(hand, word_list)
+        if word == '.':
+            print "Computer couldn't find a word for current hand!"
+            print display_score(total_score)
+            return None
+            
+        if is_valid_word(word, hand, word_list):
+            score = get_word_score(word, HAND_SIZE)
+            total_score += score
+            hand = update_hand(hand, word)
+            print '"' + word + '" earned ' + str(score) + ' points. ', display_score(total_score)
+        else:
+            print "Invalid word, please try again." 
     
 #
 # Problem #6C: Playing a game
@@ -64,8 +90,25 @@ def play_game(word_list):
 
     word_list: list (string)
     """
-    # TO DO...
-        
+    hand = deal_hand(HAND_SIZE)
+    while(True):
+        choice = raw_input("Enter 'n' for new game, 'r' to restart last game or 'e' to exit: ")
+        if choice == 'e':
+            break
+        player = raw_input("Enter 'u' for user and 'c' for computer player: ")
+        if player == 'u':
+            if choice == 'n':
+                hand = deal_hand(HAND_SIZE)
+                play_hand(hand, word_list)
+            if choice == 'r':
+                play_hand(hand, word_list)
+        if player == 'c':
+            if choice == 'n':
+                hand = deal_hand(HAND_SIZE)
+                comp_play_hand(hand, word_list)
+            if choice == 'r':
+                comp_play_hand(hand, word_list)
+
 #
 # Build data structures used for entire session and play game
 #
